e13b20b77376d035587dd143da696a5dac9d2a2f
Thomas Grainger <tom@procensus.com>
2017-12-06 15:38:26 +0000

    fix syntax error

--- compareImages.js
+++ compareImages.js
@@ -1,7 +1,7 @@
 var resemble = require('./resemble');
 
 module.exports = function(image1, image2, options) {
-  return new Promise(function(resolve, reject)) {
+  return new Promise(function(resolve, reject) {
     resemble.compare(image1, image2, options, function(err, data) {
       if (err) {
         reject(err);
